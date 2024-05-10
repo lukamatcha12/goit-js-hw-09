a7412593dcdefdab1c3f90d95291d9e05d85fb47
lukamatcha12 <148770204+lukamatcha12@users.noreply.github.com>
2024-05-10 14:36:14 +0000

    Deploying to gh-pages from @ lukamatcha12/goit-js-hw-09@aeb8c5f22b94a9a56fd2a8f2d8ff6e9c17030bec 🚀

---
+++ color.408b37a1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let a=null;d.disabled=!0,d.addEventListener("click",(function(){clearInterval(a),d.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,d.disabled=!1,e.disabled=!0}),1e3)}));
+//# sourceMappingURL=color.408b37a1.js.map
